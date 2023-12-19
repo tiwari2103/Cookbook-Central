f772dbacb565318a8ecddbb67f6e82ac958ce653
tiwari2103 <150064985+tiwari2103@users.noreply.github.com>
2023-12-19 12:44:08 +0530

    UpdateRequest(Piyush tiwari)

--- recipeBook/app/Http/Requests/Post/UpdateRequest.php
+++ recipeBook/app/Http/Requests/Post/UpdateRequest.php
@@ -11,7 +11,7 @@ class UpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,6 +21,10 @@ class UpdateRequest extends FormRequest
      */
     public function rules(): array
     {
-        
+      return [
+            'title' => 'required|string|min:3|max:250',
+            'content' => 'required|string|min:3|max:6000',
+            'featured_image' => 'nullable|image|max:1024|mimes:jpg,jpeg,png',
+        ];  
     }
 }
